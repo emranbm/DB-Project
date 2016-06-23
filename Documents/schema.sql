90b146beea69fe4f079aaf7bd148e2fff5450ee6
Mr. Coder <emran.bm@gmail.com>
2016-06-23 17:39:56 +0430

    new db dump

--- Documents/schema.sql
+++ Documents/schema.sql
@@ -1,77 +1,198 @@
-drop database if exists db_project;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_project
+-- ------------------------------------------------------
+-- Server version	5.6.17
 
-create database db_project;
-use db_project;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table insurance(
-name varchar(50) primary key);
+--
+-- Table structure for table `confidence`
+--
 
-create table patient(
-id int(10) ,
-password varchar(30),
-name varchar(30) not null,
-birthday date,
-gender varchar(6) not null,
-basic_insurance varchar(50),
-secondary_insurance varchar(50),
-degree varchar(20),
-job varchar(50),
-city varchar(20),
-history_privilege bool default false,
-primary key (id),
-foreign key (basic_insurance) references insurance (name),
-foreign key (secondary_insurance) references insurance (name)
-);
-
-create table illness(
-type varchar(50) primary key,
-security_level int(1) default 0);
-
-create table doctor(
-id int(10) primary key,
-name varchar(30) not null,
-expertise varchar(50),
-is_drugstore_keeper bool not null default false);
-
-create table confidence(
-patient_id int(10),
-doctor_id int(10),
-primary key (patient_id, doctor_id),
-foreign key (patient_id) references patient (id),
-foreign key (doctor_id) references doctor (id)
-);
-
-create table coverage(
-illness_type varchar(50),
-insurance_name varchar(50),
-coverage_percent int(3),
-primary key (illness_type, insurance_name),
-foreign key (illness_type) references illness (type),
-foreign key (insurance_name) references insurance (name)
-);
-
-create table history(
-id int(10) primary key auto_increment,
-patient_id int(10),
-doctor_id int(10),
-illness_type varchar(50),
-visit_date date,
-medicine varchar(100),
-medicine_purchased bool default false,
-doctor_diagnosis text,
-patient_description text,
-foreign key (patient_id) references patient (id),
-foreign key (doctor_id) references doctor (id),
-foreign key (illness_type) references illness (type)
-);
-
-delimiter //
-
-create procedure get_history()
+DROP TABLE IF EXISTS `confidence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `confidence` (
+  `patient_id` varchar(10) NOT NULL DEFAULT '',
+  `doctor_id` varchar(10) NOT NULL DEFAULT '',
+  `illness_type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`patient_id`,`doctor_id`),
+  KEY `doctor_id` (`doctor_id`),
+  KEY `illness_type` (`illness_type`),
+  CONSTRAINT `confidence_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`),
+  CONSTRAINT `confidence_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`),
+  CONSTRAINT `confidence_ibfk_3` FOREIGN KEY (`illness_type`) REFERENCES `illness` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 trigger on_confidence_insert before insert on confidence
+for each row
 begin
-select id, doctor_id, illness_type, visit_date, medicine, medicine_purchased, doctor_diagnosis, patient_description
-from history natural join patient natural join illness
-where (history_privilege = 1) or (security_level = 0);
-end//
 
-delimiter ;
+declare user varchar(10);
+
+select user() into user;
+set user = substring(user, 1, position('@' IN user));
+
+if (user like "3%") then
+	if (exists (select * from confidence where patient_id = user)) then
+		call error();
+	end if;
+elseif (user like "2%") then
+	call error();
+elseif (user like "1%") then
+		if (new.patient_id not in (select patient_id from confidence where doctor_id = user)) then
+			call error();
+		end if;
+end if;
+
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `coverage`
+--
+
+DROP TABLE IF EXISTS `coverage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `coverage` (
+  `illness_type` varchar(50) NOT NULL DEFAULT '',
+  `insurance_name` varchar(50) NOT NULL DEFAULT '',
+  `coverage_percent` int(3) DEFAULT NULL,
+  PRIMARY KEY (`illness_type`,`insurance_name`),
+  KEY `insurance_name` (`insurance_name`),
+  CONSTRAINT `coverage_ibfk_1` FOREIGN KEY (`illness_type`) REFERENCES `illness` (`type`),
+  CONSTRAINT `coverage_ibfk_2` FOREIGN KEY (`insurance_name`) REFERENCES `insurance` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctor` (
+  `id` varchar(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `expertise` varchar(50) DEFAULT NULL,
+  `is_drugstore_keeper` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `history`
+--
+
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `history` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `patient_id` varchar(10) DEFAULT NULL,
+  `doctor_id` varchar(10) DEFAULT NULL,
+  `illness_type` varchar(50) DEFAULT NULL,
+  `visit_date` date DEFAULT NULL,
+  `medicine` varchar(100) DEFAULT NULL,
+  `medicine_purchased` tinyint(1) DEFAULT '0',
+  `doctor_diagnosis` text,
+  `patient_description` text,
+  PRIMARY KEY (`id`),
+  KEY `patient_id` (`patient_id`),
+  KEY `doctor_id` (`doctor_id`),
+  KEY `illness_type` (`illness_type`),
+  CONSTRAINT `history_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`),
+  CONSTRAINT `history_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`),
+  CONSTRAINT `history_ibfk_3` FOREIGN KEY (`illness_type`) REFERENCES `illness` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `illness`
+--
+
+DROP TABLE IF EXISTS `illness`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `illness` (
+  `type` varchar(50) NOT NULL,
+  `security_level` int(1) DEFAULT '0',
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `insurance`
+--
+
+DROP TABLE IF EXISTS `insurance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `insurance` (
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `id` varchar(10) NOT NULL DEFAULT '',
+  `password` varchar(30) DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(6) NOT NULL,
+  `basic_insurance` varchar(50) DEFAULT NULL,
+  `secondary_insurance` varchar(50) DEFAULT NULL,
+  `degree` varchar(20) DEFAULT NULL,
+  `job` varchar(50) DEFAULT NULL,
+  `city` varchar(20) DEFAULT NULL,
+  `history_privilege` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `basic_insurance` (`basic_insurance`),
+  KEY `secondary_insurance` (`secondary_insurance`),
+  CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`basic_insurance`) REFERENCES `insurance` (`name`),
+  CONSTRAINT `patient_ibfk_2` FOREIGN KEY (`secondary_insurance`) REFERENCES `insurance` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-23 17:29:31

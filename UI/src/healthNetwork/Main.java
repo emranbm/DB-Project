1b4710aec374c79f6f1924e024a03b7f586948b4
mmaghajani <mma137421@gmail.com>
2016-06-24 10:44:48 +0430

    Adds main class

---
+++ UI/src/healthNetwork/Main.java
@@ -0,0 +1,11 @@
+package healthNetwork;
+
+/**
+ * Created by mma on 6/8/2016.
+ */
+public class Main {
+
+    public static void main( String[] args){
+        StartPage startPage = new StartPage() ;
+    }
+}
